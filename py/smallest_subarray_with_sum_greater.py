3e1ea50b625419f9f0b36ce157b2cc42060992fb
yogesh-b <yogeshbavishi199@gmail.com>
2025-09-02 22:40:34 +0530

    smallest subarray with sum greater than - bruteforce WIP

--- py/smallest_subarray_with_sum_greater.py
+++ py/smallest_subarray_with_sum_greater.py
@@ -13,3 +13,28 @@
 ##note: subarray means consecutive elements
 
 
+
+#brute force method
+#input array and target x
+arr = [1,5,3,8,4,6,55,22,15,45]
+x = 100
+smallest_sub_len = 0
+#main logic below
+arr_n = len(arr)
+for i in range(len(arr)):
+    # we need two indices... one goes left, one goes right from the current index
+    n = i
+    m = i
+    tmp_sum = 0
+    tmp_arr = arr[n:m]
+    while(n>0 or m < arr_n-1):
+        if n>0:
+            n-=1
+            tmp_arr = arr[n:m+1]
+            print(tmp_arr)
+        if m<arr_n-1:
+            m+=1
+            tmp_arr = arr[n:m+1]
+            print(tmp_arr)
+        print(arr)
+        #WIP
