a593769b5ad353dc6a8eda11a9c8a390ae85f6e0
yogesh-b <yogesh@>
2025-09-02 21:57:08 +0530

    first prog -init

---
+++ py/smallest_subarray_with_sum_greater.py
@@ -0,0 +1,15 @@
+#Smallest subarray with sum greater than x
+#Given a number x and an array of integers arr, find the smallest subarray with sum greater than the given value. If such a subarray do not exist return 0 in that case.
+
+#Examples:
+
+#Input: x = 51, arr[] = [1, 4, 45, 6, 0, 19]
+#Output: 3
+#Explanation: Minimum length subarray is [4, 45, 6]
+#Input: x = 100, arr[] = [1, 10, 5, 2, 7]
+#Output: 0
+#Explanation: No subarray exist
+
+##note: subarray means consecutive elements
+
+
